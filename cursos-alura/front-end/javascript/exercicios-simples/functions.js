37e58c0ac0cb343e80ab44cacc6a7a6046afc2f5
fredericofaleiro <fredericofaleito@gmail.com>
2026-03-18 14:18:18 -0300

    treinando mais funçoes

--- cursos-alura/front-end/javascript/exercicios-simples/functions.js
+++ cursos-alura/front-end/javascript/exercicios-simples/functions.js
@@ -1,20 +1,36 @@
+//calcula fatorial
 const fatorial = function f(numb) {
-   if (numb === 0 || numb === 1) return 1;
-   return numb * f(numb - 1)
-}
+  if (numb === 0 || numb === 1) return 1;
+  return numb * f(numb - 1);
+};
 
-console.log(fatorial(5))
+console.log(fatorial(5));
 
+//calcula juros compostos
 const jurosCompostos = (valor, juros, tempo) => {
-   let taxaJuros = juros/100 + 1;
-   return valor * Math.pow(taxaJuros, tempo)
-}
+  let taxaJuros = juros / 100 + 1;
+  return valor * Math.pow(taxaJuros, tempo);
+};
 
-console.log(jurosCompostos(1000, 10, 2))
+console.log(jurosCompostos(1000, 10, 2));
 
+//calcula desconto
 const descontoProduto = (valor, porcentagem) => {
-   let taxaDesconto = porcentagem / 100
-   return resultado = valor - (taxaDesconto * valor)
+  let taxaDesconto = porcentagem / 100;
+  return valor - taxaDesconto * valor;
+};
+
+console.log(descontoProduto(120, 50));
+
+//calcula media
+const media = (numb1, numb2) => (numb1 + numb2) / 2;
+
+console.log(media(2, 4));
+
+//verificar paridade
+const verificarParidade = (numb) => {
+   if (numb % 2 === 0) return "par";
+   return "impar"
 }
 
-console.log(descontoProduto(120, 50))
+console.log(verificarParidade(1))
